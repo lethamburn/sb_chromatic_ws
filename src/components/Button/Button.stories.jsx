1aefdb76e8f6a633486ab9e29e2ad0b980bfe0bb
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2024-02-13 20:22:55 +0100

    feat initial commit

---
+++ src/components/Button/Button.stories.jsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+export default {
+  title: "components/Button",
+  component: Button,
+  args: {
+    children: "Button",
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  type: "secondary",
+  children: "Secondary"
+};
+
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  type: "tertiary",
+  children: "Tertiary"
+};
